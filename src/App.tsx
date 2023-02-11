8dfc1a633fd8bd8ad4f1b03a4643883751c04ea6
Stepan Osadchii <s.osadchiy@freematiq.com>
2023-02-11 11:21:35 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import diamond from "./img/diamond.png";
+import "./style/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section__form">
+      <div className="container">
+        <div className="card__inner-wrapper">
+          <div className="card">
+            <div className="card__inside-wrapper">
+              <h3 className="card__header">
+                Mint NFT and get money to <br /> your wallet during 1 hour
+              </h3>
+              <div className="card__image">
+                <img src={diamond} alt="diamond" />
+              </div>
+              <a href="#" className="card__button">
+                {" "}
+                MINT TICKET 0.059 ETH{" "}
+              </a>
+              <div className="card__footer-wrapper">
+                <div className="card__num-container">
+                  <div>8</div>
+                  <div>7</div>
+                  <div>7</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
